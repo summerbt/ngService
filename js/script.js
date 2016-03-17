37599d5720b4da6110fa7b79d4b9163487af5be8
Summer Barton-Taylor <summerbartontaylor@gmail.com>
2016-03-16 19:58:59 -0500

    updated for mentor session

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 angular.module("ngService", [])
+    .controller('MealController', function ($scope) {
+        $scope.calc = mealCalc;
+        //        $scope.calc.taxDollars = $scope.calc.base * ($scope.calc.tax / 100);
+        //        $scope.calc.subtotal = $scope.calc.base + $scope.calc.taxDollars;
+        //        $scope.calc.tipDollars = $scope.calc.subtotal * ($scope.calc.tip / 100);
+
+    });
+var mealCalc = {
+    tax: 0,
+    taxDollars: 0,
+    tip: 0,
+    tipDollars: 0,
+    base: 0,
+    subtotal: 0,
+}
