6209652aa57df2f805dcdfd9cf933dc7ef6df5c7
Summer Barton-Taylor <summerbartontaylor@gmail.com>
2016-03-19 12:09:42 -0500

    it works

--- js/script.js
+++ js/script.js
@@ -1,16 +1,53 @@
 angular.module("ngService", [])
-    .controller('MealController', function ($scope) {
-        $scope.calc = mealCalc;
-        //        $scope.calc.taxDollars = $scope.calc.base * ($scope.calc.tax / 100);
-        //        $scope.calc.subtotal = $scope.calc.base + $scope.calc.taxDollars;
-        //        $scope.calc.tipDollars = $scope.calc.subtotal * ($scope.calc.tip / 100);
+    .controller('MealController', ['$scope', controller]);
 
-    });
-var mealCalc = {
-    tax: 0,
-    taxDollars: 0,
-    tip: 0,
-    tipDollars: 0,
-    base: 0,
-    subtotal: 0,
-}
+//    .controller('SettingsController2', ['$scope', SettingsController2]);
+function controller($scope) {
+    $scope.taxDollars = 0;
+    $scope.tax = 0;
+    $scope.tip = 0;
+    $scope.tipDollars = 0;
+    $scope.base = 0;
+    $scope.subtotal = 0;
+    $scope.earningsTipTotal = 0;
+    $scope.earningsMealCount = 0;
+    $scope.earningsAvgTip = 0;
+
+    $scope.handleSubmit = function () {
+        console.log("handleSubmit");
+        $scope.taxDollars = $scope.base * ($scope.tax / 100);
+        $scope.subtotal = $scope.base + $scope.taxDollars;
+        $scope.tipDollars = $scope.subtotal * ($scope.tip / 100);
+        $scope.earningsTipTotal += $scope.tipDollars;
+        $scope.earningsMealCount += 1
+        $scope.earningsAvgTip = $scope.earningsTipTotal / $scope.earningsMealCount;
+        console.log($scope.taxDollars);
+        $scope.base = 0;
+        $scope.tax = 0;
+        $scope.tip = 0;
+    }
+
+    $scope.handleCancel = function () {
+        $scope.base = 0;
+        $scope.tax = 0;
+        $scope.tip = 0;
+        $scope.subtotal = 0;
+        $scope.tipDollars = 0;
+    }
+    $scope.handleReset = function () {
+        $scope.handleCancel();
+        $scope.earningsTipTotal = 0;
+        $scope.earningsMealCount = 0;
+        $scope.earningsAvgTip = 0;
+    }
+};
+
+//var mealCalc = {
+//    tax: 0,
+//    taxDollars: 0,
+//    tip: 0,
+//    tipDollars: 0,
+//    base: 0,
+//    subtotal: 0,
+//    tipTotal: 0,
+//}
